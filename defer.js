63c6627b48d7cbf76ea9ded6e72ed59fa2a8526a
Harikrishnan <harikrishnan.a@infibeam.net>
2013-07-05 18:21:53 +0530

    Improved Performance of Javascript(defer.js) by iterating over elements with className as 'active' rather than 'menu-item'

--- defer.js
+++ defer.js
@@ -2,7 +2,12 @@ var menuItems = document.getElementsByClassName("menu-item");
 for (var i=0;i<menuItems.length;i++)
 {
   menuItems[i].onclick=function(event){
-      var menuItemList= menuItems;
+      /*var menuItemList= menuItems;
+      for (var i=0;i<menuItemList.length;i++)
+      {
+      menuItemList[i].classList.remove('active');
+      }*/
+      var menuItemList = document.getElementsByClassName('active');
       for (var i=0;i<menuItemList.length;i++)
       {
       menuItemList[i].classList.remove('active');
